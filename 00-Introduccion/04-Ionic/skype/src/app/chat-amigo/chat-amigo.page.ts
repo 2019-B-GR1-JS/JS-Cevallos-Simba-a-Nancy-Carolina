b5acfa1c0a028caa7a74bd06ce6c2555800d3346
CaroCe <carito_kt@hotmail.com>
2020-01-06 16:21:34 -0500

    deber app

--- 00-Introduccion/04-Ionic/skype/src/app/chat-amigo/chat-amigo.page.ts
+++ 00-Introduccion/04-Ionic/skype/src/app/chat-amigo/chat-amigo.page.ts
@@ -8,10 +8,147 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ChatAmigoPage implements OnInit {
   argumento = null;
-
-  mensajes = [
-    {}
-  ]
+  contactos = [
+    {
+      id: 1,
+      nombre: 'Sara Selena',
+      mensajes: [{
+        clase: 0,
+        tiempo: '10:23',
+        mensaje: 'puedes enviarme la nota'
+      }, {
+        clase: 1,
+        tiempo: '11:10',
+        mensaje: 'gracias te debo una'
+      }, {
+        clase: 2,
+        tiempo: '10:00',
+        mensaje: 'Hola como estas'
+      }
+      ],
+      imagen: 'https://i.pinimg.com/originals/1c/da/6d/1cda6db0c845f3d486f6d13fca9f3e3e.png',
+      tiempo: '21:45.'
+    },
+    {
+      id: 2,
+      nombre: 'German simbaña',
+      mensajes: [{
+        clase: 0,
+        tiempo: '10:23',
+        mensaje: 'gracias'
+      }
+      ],
+      imagen: 'http://assets.cdnelnuevodiario.com/ckeditor/2017/08/01/tom-halland.png',
+      tiempo: 'En línea'
+    },
+    {
+      id: 3,
+      nombre: 'RDC-Mosflor',
+      mensajes: [{
+        clase: 0,
+        tiempo: '10:23',
+        mensaje: 'puedes enviarme la nota'
+      }, {
+        clase: 1,
+        tiempo: '11:10',
+        mensaje: 'gracias te debo una'
+      }, {
+        clase: 2,
+        tiempo: '10:00',
+        mensaje: 'Hola como estas'
+      }
+      ],
+      imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuMmV03b7xjb5WTclX7P1Aq2CbB2md4Yt5bP_gI6i86M7e2icE&s',
+      tiempo: '12/12/2019.'
+    },
+    {
+      id: 4,
+      nombre: '12 razones',
+      mensajes: [{
+        clase: 0,
+        tiempo: '10:23',
+        mensaje: 'sin respuesta'
+      }, {
+        clase: 1,
+        tiempo: '11:10',
+        mensaje: 'gracias te debo una'
+      }, {
+        clase: 2,
+        tiempo: '10:00',
+        mensaje: 'Hola como estas'
+      }
+      ],
+      imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE7N_Zf5qAOAsYAjVYRYXsjJk57csMhqHktwr9Mi69MQ4ednwF&s',
+      tiempo: '25/12/2019'
+    },
+    {
+      id: 5,
+      nombre: 'Adam Sandles',
+      mensajes: [{
+        clase: 0,
+        tiempo: '10:23',
+        mensaje: 'tienes los apuntes'
+      }, {
+        clase: 1,
+        tiempo: '11:10',
+        mensaje: 'gracias'
+      }, {
+        clase: 2,
+        tiempo: '10:00',
+        mensaje: 'quien es?'
+      }
+      ],
+      imagen: 'http://elheraldoslp.com.mx/wp-content/uploads/2015/04/adam-sandler-net-worth.png',
+      tiempo: '10/11/2019'
+    },
+    {
+      id: 6,
+      nombre: 'Luciano',
+      mensajes: [{
+        clase: 0,
+        tiempo: '10:23',
+        mensaje: 'adivina'
+      }, {
+        clase: 1,
+        tiempo: '11:10',
+        mensaje: 'mi pelicula favorita'
+      }
+      ],
+      imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHpvwYWTnDMGrfrQLqSfWIgtv7e39mmyHecvoj1ff_9y_4BYE4&s',
+      tiempo: '3/10/2019'
+    }
+  ];
+  respuesta = [
+    {
+      clase: 'otro-mensaje',
+      tiempo: '10.23',
+      mensaje: 'dime en que puedo ayudarte'
+    }, {
+      clase: 'otro-mensaje',
+      tiempo: '10.23',
+      mensaje: 'claro te envio enseguida'
+    }, {
+      clase: 'otro-mensaje',
+      tiempo: '10.23',
+      mensaje: 'espera'
+    }, {
+      clase: 'otro-mensaje',
+      tiempo: '10.23',
+      mensaje: 'ok'
+    }, {
+      clase: 'otro-mensaje',
+      tiempo: '10.23',
+      mensaje: 'gracias'
+    }, {
+      clase: 'otro-mensaje',
+      tiempo: '10.23',
+      mensaje: 'nos vemos'
+    }, {
+      clase: 'otro-mensaje',
+      tiempo: '10.23',
+      mensaje: 'hola como estas'
+    }
+  ];
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
--- 00-Introduccion/04-Ionic/skype/src/app/modal-chat/modal-chat.page.ts
+++ 00-Introduccion/04-Ionic/skype/src/app/modal-chat/modal-chat.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ModalController} from '@ionic/angular';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-modal-chat',
@@ -8,8 +10,52 @@ import {ModalController} from '@ionic/angular';
 })
 export class ModalChatPage implements OnInit {
 
-  constructor(private modalCtrl: ModalController) { }
-
+  constructor(private modalCtrl: ModalController,
+              private router: Router) { }
+  contactos = [
+    {
+      id: 0,
+      nombre: 'Sara Selena',
+      mensaje: 'hola como estas',
+      imagen: 'https://i.pinimg.com/originals/1c/da/6d/1cda6db0c845f3d486f6d13fca9f3e3e.png',
+      ultima_conexion: 'vie.'
+    },
+    {
+      id: 1,
+      nombre: 'German simbaña',
+      mensaje: 'gracias',
+      imagen: 'http://assets.cdnelnuevodiario.com/ckeditor/2017/08/01/tom-halland.png',
+      ultima_conexion: 'jue.'
+    },
+    {
+      id: 2,
+      nombre: 'RDC-Mosflor',
+      mensaje: 'sin respuesta',
+      imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuMmV03b7xjb5WTclX7P1Aq2CbB2md4Yt5bP_gI6i86M7e2icE&s',
+      ultima_conexion: '12/12/2019.'
+    },
+    {
+      id: 3,
+      nombre: '12 razones',
+      mensaje: 'quien es?',
+      imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE7N_Zf5qAOAsYAjVYRYXsjJk57csMhqHktwr9Mi69MQ4ednwF&s',
+      ultima_conexion: '25/12/2019'
+    },
+    {
+      id: 4,
+      nombre: 'Adam Sandles',
+      mensaje: 'mi pelicula favorita',
+      imagen: 'http://elheraldoslp.com.mx/wp-content/uploads/2015/04/adam-sandler-net-worth.png',
+      ultima_conexion: '10/11/2019'
+    },
+    {
+      id: 5,
+      nombre: 'Luciano',
+      mensaje: 'actor',
+      imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHpvwYWTnDMGrfrQLqSfWIgtv7e39mmyHecvoj1ff_9y_4BYE4&s',
+      ultima_conexion: '3/10/2019'
+    }
+  ];
   ngOnInit() {
   }
 
@@ -17,4 +63,8 @@ export class ModalChatPage implements OnInit {
     this.modalCtrl.dismiss();
   }
 
+  abrirChat(id) {
+    this.modalCtrl.dismiss();
+    this.router.navigate(['/chat-amigo', id]);
+  }
 }
